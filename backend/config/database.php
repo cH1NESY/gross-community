5cdbc6fb85291e286e40deb5c8a273635e3e4c3f
Чингис <Underchina@mail.ru>
2025-11-26 17:58:50 +0800

    fix 500

--- backend/config/database.php
+++ backend/config/database.php
@@ -86,8 +86,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'host' => env('POSTGRES_HOST') ?: env('DB_HOST', '127.0.0.1'),
+            'port' => env('POSTGRES_PORT') ?: env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
